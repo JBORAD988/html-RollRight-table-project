ef3eb51d66dc73658220919f816749489c80bf25
JBorad988 <jayborad366@gmail.com>
2024-05-22 16:14:09 +0530

    init

---
+++ src/app/role-rights/role-rights.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-role-rights',
+  templateUrl: './role-rights.component.html',
+  styleUrls: ['./role-rights.component.scss']
+})
+export class RoleRightsComponent {
+
+}
